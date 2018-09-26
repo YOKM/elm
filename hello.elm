041e0bd6814cc7947fc49d2aaa67072df5f8eee5
Gaston <cparwez@gmail.com>
2018-09-26 10:07:46 +0100

    Test

---
+++ hello.elm
@@ -0,0 +1,25 @@
+import Browser
+import Html exposing (Html, button, div, text)
+import Html.Events exposing (onClick)
+
+main =
+  Browser.sandbox { init = 0, update = update, view = view }
+
+type Msg = Increment | Decrement
+
+update msg model =
+  case msg of
+    Increment ->
+      model + 5
+
+    Decrement ->
+      model - 3
+
+view model =
+  div []
+    [ button [ onClick Decrement ] [ text "-" ]
+    , div [] [ text (String.fromInt model) ]
+    , button [ onClick Increment ] [ text "+" ]
+    ]
+  
+            
